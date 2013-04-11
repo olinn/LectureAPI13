a4c8a039ebf3e3e67d7857ccef1a38032b3c4752
Kristjan Broder Lund <kristjan.1234@gmail.com>
2013-04-11 20:39:35 +0000

    Links Comment and User

--- BasicProject/MvcAngular.Web/Models/Comment.cs
+++ BasicProject/MvcAngular.Web/Models/Comment.cs
@@ -8,7 +8,7 @@ namespace MvcAngular.Web.Models
     public class Comment
     {
         public int ID { get; set; }
-        public int UserID { get; set; }
+        public virtual User User { get; set; }
         public string CommentText { get; set; }
         public DateTime CommentTime { get; set; }
         public virtual Lecture Lecture { get; set; }
