678f3298f92ad2419a8169106d88612f74cfd26b
Kristjan Broder Lund <kristjan.1234@gmail.com>
2013-04-12 00:45:48 +0000

    Moved time formating to a filter

--- BasicProject/MvcAngular.Web/Scripts/app/services.js
+++ BasicProject/MvcAngular.Web/Scripts/app/services.js
@@ -10,3 +10,20 @@
             update: { method: 'PUT' }
         });
     })
+
+    .filter('timeAgo', function () {
+        return function (input) {
+            var pubAgo = Date.now() - new Date(input).getTime();
+
+            var minSince = Math.floor(pubAgo / 60000);
+            var hrsSince = Math.floor(minSince / 60);
+            var daysSince = Math.floor(hrsSince / 24);
+
+            if (daysSince > 0)
+                return daysSince + ' days ago';
+            else if (hrsSince > 0)
+                return hrsSince + ' hours ago';
+            else if (minSince != 0)
+                return minSince + ' minutes ago';
+        }
+    })
